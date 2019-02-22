3a6b1d35c209486358d40f6936a3813b90006743
edabdala <40128369+edabdala@users.noreply.github.com>
2019-02-22 11:20:31 -0600

    Add files via upload

---
+++ Table Definitions/ct_Actions_Transactions.sql
@@ -0,0 +1,10 @@
+create table Actions_allTime-Transactions.csv (
+	transaction_id varchar(20),
+	account_id varchar(8),
+	transaction_amount float(17,2),
+	client_id varchar(10),
+	transaction_status varchar(3),
+	[login] varchar(10),
+	transaction_date datetime
+	reporting_period varchar(8)
+)
