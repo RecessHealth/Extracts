acd15cbe726f89a1103fbd44c468c230ca220538
Jay <marketing@pfsgroup.org>
2019-02-26 15:21:31 -0600

    ~ extract_Demographics_currentInventory.sql : add inventory age = function of (cancelled_date | today)

--- extract_Demographics_currentInventory.sql
+++ extract_Demographics_currentInventory.sql
@@ -1,5 +1,5 @@
-; declare @date date = (select getdate()-1) -- date filter = yesterday
-select cast(@date as datetime) -- format to match [assigned_date] datetime type at 00:00:00 
+--; declare @date date = (select getdate()-1) -- date filter = yesterday
+--select cast(@date as datetime) -- format to match [assigned_date] datetime type at 00:00:00 
 
 select 
 --	med_rec_num as medical_record, 
@@ -8,6 +8,10 @@ select
 	client_id,
 	datediff(year, pt_dob,getdate()) as patient_age_exported,
 	getdate() as export_date,
+	case
+		when cancelled_date is null then abs(datediff(day, getdate(), assigned_date))
+		else abs(datediff(day, cancelled_date, assigned_date)) end 
+		as inventory_age, 
 	account_zip, 
 	curr_fc as financial_class,
 	pat_type as patient_type,
@@ -29,8 +33,11 @@ select
 	total_balance,
 	ttl_pt_pmt,
 	cancelled_amount 
+into #TempBarnInventoryDems
 from tbl_Report_Data 
 where 
-	client_id like 'BH%' and 
-	assigned_date = @date
+	client_id like 'BH%' 
+--	and 
+--	assigned_date = @date
 ;
+
