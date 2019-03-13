93952141de55b266e429be9454e8df654311c5cc
Jay <marketing@pfsgroup.org>
2019-03-13 09:21:17 -0500

    s_sql_q_%.sql: stop tracking duplicate extract files

--- Source SQL Queries/s_sql_q_Reference_StrataCodes.sql
+++
@@ -1,5 +0,0 @@
-select 
-	termcd as [action_code],
-	termdesc as [description],
-	systemtc as [system_generated] 
-from termcd
--- Source SQL Queries/s_sql_q_join_Clientcode_Dialer_campaign.sql
+++
@@ -1,5 +0,0 @@
-select a.project, campaign_id, campaign
-from Stratadial.dbo.tbl_project a
-inner join tbl_CampaignMapping c
-	on a.campaignid = c.mapping_name
-order by project asc; 
